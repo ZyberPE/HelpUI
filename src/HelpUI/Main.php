1b28460d52a606329be5e1ee44d7b36ea346ca88
ZyberPE <zyberpe@gmail.com>
2026-03-16 16:39:09 -0400

    Update Main.php

--- src/HelpUI/Main.php
+++ src/HelpUI/Main.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace HelpUI;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandMap;
 
 class Main extends PluginBase{
 
@@ -14,12 +15,30 @@ class Main extends PluginBase{
 
         $commandMap = $this->getServer()->getCommandMap();
 
-        $existing = $commandMap->getCommand("help");
-
-        if($existing !== null){
-            $commandMap->unregister($existing);
-        }
+        $this->unregisterHelp($commandMap);
 
         $commandMap->register("helpui", new HelpCommand($this));
+    }
+
+    private function unregisterHelp(CommandMap $commandMap): void{
+
+        $command = $commandMap->getCommand("help");
+
+        if($command !== null){
+            $commandMap->unregister($command);
+        }
+
+        $knownCommands = $commandMap->getKnownCommands();
+
+        foreach($knownCommands as $name => $cmd){
+            if($name === "help"){
+                unset($knownCommands[$name]);
+            }
+        }
+
+        $reflection = new \ReflectionClass($commandMap);
+        $property = $reflection->getProperty("knownCommands");
+        $property->setAccessible(true);
+        $property->setValue($commandMap, $knownCommands);
     }
 }
